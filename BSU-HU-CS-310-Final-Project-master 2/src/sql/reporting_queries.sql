e910f869b8ca8ec7be34abbb2891460e50262068
ryliewalsh <62161188+ryliewalsh@users.noreply.github.com>
2023-12-12 19:13:03 -0700

    Delete BSU-HU-CS-310-Final-Project-master 2 directory

--- BSU-HU-CS-310-Final-Project-master 2/src/sql/reporting_queries.sql
+++
@@ -1,8 +0,0 @@
-/* Put your final project reporting queries here */
-USE cs_hu_310_final_project;
-
--- Example (remove before submitting)
--- Get all students
-SELECT
-    *
-FROM students;
--- BSU-HU-CS-310-Final-Project-master 2/src/sql/schema.sql
+++
@@ -1,6 +0,0 @@
-/*
-  Put all table create statements in this file as well as any UDFs or stored
-  procedures.
-
-  NOTE: This file must be able to run repetitively without any errors
-*/
